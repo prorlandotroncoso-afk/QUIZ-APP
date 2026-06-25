85cf22e1c24aa6c7ff07ce112c0e4b987b35f89b
prorlandotroncoso-afk <prorlandotroncoso@gmail.com>
2026-06-25 15:03:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -188,29 +188,32 @@ def submit():
          "Fecha": datetime.now().strftime("%Y-%m-%d %H:%M")
     }
 
-    try:
+      try:
         r = requests.post(
-        GOOGLE_SCRIPT_URL,
-        data={
-            "fecha": data["Fecha"],
-            "nombre": data["Nombre"],
-            "email": data["Email"],
-            "equipo": data["Equipo"],
-            "puntaje": data["Puntaje"],
-            "total": data["Total"],
-            "porcentaje": data["Porcentaje"]
-        },
-        timeout=10
+            GOOGLE_SCRIPT_URL,
+            data={
+                "fecha": data["Fecha"],
+                "nombre": data["Nombre"],
+                "email": data["Email"],
+                "equipo": data["Equipo"],
+                "puntaje": data["Puntaje"],
+                "total": data["Total"],
+                "porcentaje": data["Porcentaje"]
+            },
+            timeout=10
         )
 
         print("GOOGLE STATUS:", r.status_code)
         print("GOOGLE RESPONSE:", r.text)
 
-return f"""
-<h2>DEBUG</h2>
-<p>Status Google: {r.status_code}</p>
-<pre>{r.text}</pre>
-"""
+    except Exception as e:
+        print("ERROR SHEETS:", e)
+
+    return f"""
+    <h2>DEBUG</h2>
+    <p>Status Google: {r.status_code}</p>
+    <pre>{r.text}</pre>
+    """
 
     except Exception as e:
         print("ERROR SHEETS:", e)
